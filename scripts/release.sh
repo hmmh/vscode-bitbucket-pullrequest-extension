8c03a9887571927a76072b42b2d965f0ca2aed51
Thorben Ziegler <thorben.ziegler@hmmh.de>
2023-10-11 14:30:13 +0200

    feat(release): Improve release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,7 +4,12 @@
 
 # get version from package.json
 version=$(node -p -e "require('./package.json').version")
+
+# show prompt for inserting release notes
+read -p "Release Notes: " release_notes
+
 # create tag with version
-git tag -a $version -m "Release $version"
+git tag -a $version -m "Release $version\n\n$release_notes"
+
 # push tag to github
 git push origin $version
